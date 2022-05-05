d2bed697a9c54fd1c3b229e03d8524642066df83
Niveytha Waran <niveytha@gmail.com>
2022-05-05 17:43:58 +0800

    Preload script

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const { app, BrowserWindow } = require('electron') // controls application's event lifecycle
+const path = require('path')
 
 // creates and manages application windows
 const createWindow = () => {
     const win = new BrowserWindow({
       width: 800,
-      height: 600
+      height: 600,
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+        // _dirname: points to the path of the currently executing script
+        // path.join:  joins multiple path segments together, creating a combined path string that works across all platforms
+      }
     })
   
     win.loadFile('index.html')
